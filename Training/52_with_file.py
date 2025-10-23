4ea540841d7542c984e4a0fe2f5a6f9e8fb660fb
MateuszN2025 <mateusz.niedziolka@gmail.com>
2025-10-23 19:45:23 +0200

    with file w

--- Training/52_with_file.py
+++ Training/52_with_file.py
@@ -4,3 +4,15 @@ with open('file1.txt','r') as f1:
 
 print(content1)
 print(content2)
+
+file_table = []
+i=0
+
+with open('file1.txt', 'w') as f2:
+    for line in range(1000):
+        # if line / 10 == 0:
+        #     f2.write(" " + str(line))
+        if line / 10 != 0:
+            f2.write(" " + str(line))
+        else:
+            f2.write("\n")
