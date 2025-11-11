28d635e5dd131a9d26628b0fffa9993d290cdb19
MateuszN2025 <mateusz.niedziolka@gmail.com>
2025-11-11 11:15:35 +0100

    1_30 excercises

---
+++ Training/100_1_30_python_interview_ex.py
@@ -0,0 +1,15 @@
+# --- format() method ---
+# --- old-style % formatting ---
+# Example 1: enumerate()
+# 2️⃣ map() — apply a function to each element
+# filter
+# reduce
+#__repr__
+#__dict__
+#abstartmethod
+# 27. Demonstrate multiple inheritance and MRO (method resolution order).
+# 28. What is duck typing in Python?
+# 29. Implement operator overloading (e.g., __add__).
+# 30. Create a singleton pattern using a class.
+# singleton pattern with decorator
+
