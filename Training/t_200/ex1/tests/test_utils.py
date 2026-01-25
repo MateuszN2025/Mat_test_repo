2d616ba3b9a5c2eaf8ec1333b3085cd9d0c1f921
MateuszN2025 <mateusz.niedziolka@gmail.com>
2026-01-25 17:28:08 +0100

    test missing dot

--- Training/t_200/ex1/tests/test_utils.py
+++ Training/t_200/ex1/tests/test_utils.py
@@ -1,4 +1,7 @@
 from src.utils import is_valid_email
 
 def test_1():
-    assert is_valid_email('sd@') == True
+    assert is_valid_email('sd@.com') == True
+
+def test_missing_dot():
+    assert is_valid_email("user@examplecom") is False
