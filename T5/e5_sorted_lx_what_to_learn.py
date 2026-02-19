d5a882da9d56c5aea8b3b04f04920e9b29cfd6eb
Mateusz Niedziolka <mateusz.niedziolka@gmail.com>
2026-02-19 12:18:21 +0100

    exe before lx3

---
+++ T5/e5_sorted_lx_what_to_learn.py
@@ -0,0 +1,22 @@
+# ship engines
+# black box
+# gathering info from sensors
+# software system
+# gathers info about behavior
+# 700 signals
+# python
+# db flux, sgl, sql dynamodb, aws
+# aws green grass unique
+# fast api
+# multiprocessing multithreading
+
+# ==============================
+# multiprocessing
+# multithreading
+# ==============================
+
+# Version A
+a = [x for x in sorted(range(10_001)) if x % 2 == 1]
+
+# Version B
+b = sorted([x for x in range(10_001) if x % 2 == 1]) # faster
