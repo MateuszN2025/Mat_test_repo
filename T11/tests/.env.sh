d75a593cb13100cfc4059f6b52282675e0fbdde0
MateuszN2025 <mateusz.niedziolka@email.com>
2026-05-05 15:27:45 +0200

    jenkins test flow

--- T11/tests/.env.sh
+++
@@ -1,9 +0,0 @@
-# .sh without shebang for sourced snippets
-
-export CALC_SSH_PASSWORD='changeme1@'
-export CALC_SSH_USER='vboxuser1'
-export CALC_SSH_HOST='192.168.0.152'
-export CALC_REMOTE_PATH='/home/vboxuser1/calc2/b_calc'
-
-# CALC_SSH_PASSWORD='x' sets a shell variable only.
-# export CALC_SSH_PASSWORD='x' sets an environment variable inherited by child processes.
