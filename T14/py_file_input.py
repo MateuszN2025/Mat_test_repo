e34e3377de4e3884f98841237eb5989aef47af03
MateuszN2025 <mateusz.niedziolka@email.com>
2026-05-08 15:57:01 +0200

    py file input

---
+++ T14/py_file_input.py
@@ -0,0 +1,12 @@
+# 2. ls - Exercise 2:
+# List files in a target directory sorted by size from largest to smallest.
+"""
+ls -lS | grep '^-'
+"""
+
+# 2. ls - Exercise 2:
+# List files in a target directory sorted by size from largest to smallest.
+"""
+ls -lS | grep '^-'
+"""
+
