ebdf24ddb40b492854065c5424648edf30953b18
MateuszN2025 <mateusz.niedziolka@email.com>
2026-05-11 16:48:03 +0200

    api db added

--- T15/manual_tests.py
+++ T15/manual_tests.py
@@ -26,29 +26,42 @@ def main():
         "is_active": True
     }
     
+    payload3 = {
+        "id": 125,
+        "name": "Mouse",
+        "price": 10,
+        "tags": ["electronics"],
+        "is_active": True
+    }
+    
     print("------------------------------------------")
-    print(requests.post(url=f"{URL + ITEMS}", json=payload1).status_code)
-    print(requests.post(url=f"{URL + ITEMS}", json=payload2).status_code)
+    # print(requests.post(url=f"{URL + ITEMS}", json=payload1).status_code)
+    # print(requests.post(url=f"{URL + ITEMS}", json=payload2).status_code)
+    # print(requests.post(url=f"{URL + ITEMS}", json=payload3).status_code)
     
   
     
     print("------------------------------------------")
     id1 = f"{URL + ITEMS}" + "1"
-    response3 = requests.get(id1)
-    print(json.dumps((response3.json()), indent=4))
-    print(response3.status_code)
+    # response3 = requests.get(id1)
+    # print(json.dumps((response3.json()), indent=4))
+    # print(response3.status_code)
     
     print("------------------------------------------")
-    id2 = f"{URL + ITEMS}" + "15"
-    response4 = requests.get(id2)
-    print(json.dumps((response4.json()), indent=4))
-    print(response4.status_code)
+    # id2 = f"{URL + ITEMS}" + "15"
+    # response4 = requests.get(id2)
+    # print(json.dumps((response4.json()), indent=4))
+    # print(response4.status_code)
+    
+    
     
     print("------------------------------------------")
-    id3 = f"{URL + ITEMS}"
-    response5 = requests.get(id3)
-    print(json.dumps((response5.json()), indent=4))
-    print(response5.status_code)
+    id_all = f"{URL + ITEMS}"
+    response_all = requests.get(id_all)
+    print(json.dumps((response_all.json()), indent=4))
+    print(response_all.status_code)
+    
+    requests.patch(id1, data={})
     
     
 main()
