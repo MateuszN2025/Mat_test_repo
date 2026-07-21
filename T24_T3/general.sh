1d4cc7992c911bbdfc47d9466e96d123ba6de82c
MateuszN2025 <mateusz.niedziolka@email.com>
2026-07-21 21:37:26 +0200

    bash 90pc

--- T24_T3/general.sh
+++ T24_T3/general.sh
@@ -16,12 +16,12 @@
 # Use bash when you want a clean, isolated execution environment
 
 # command="${1:-GET}"
-echo "----------"
-func(){
+echo "-----func-----"
+ function func(){
     local pwd1="${1:-$(pwd)}"
     echo "$pwd1"
 }
-# func 
+func 
 echo "----if------"
 param=5
 if [[ $param -gt 4 ]]; then
@@ -30,8 +30,13 @@ else
     echo "young child"
 fi
 
-echo "-----for-----"
+echo "-----for simple-----"
 for i in 1 2 3 4; do
+    echo "$i"
+done
+
+echo "-----for medium-----"
+for ((i=;i<10;i++)); do
     echo "$i"
 done
 
@@ -42,6 +47,12 @@ while [[ counter1 -gt 0 ]]; do
     counter1=$((counter1-1))
 done
 
+
+echo "-----while one line-----"
+counter111=3
+while [[ $counter111 -gt 0 ]]; do date | awk '{print $4}'; \
+ counter111=$((counter111-1)); sleep 0.1; done
+
 # CC=5
 # while [[ $CC -gt 0 ]]; do
 #     date 
@@ -49,15 +60,111 @@ done
 #     CC=$((CC-1))
 # done 
 echo "-----read-----"
+while read -r line; do
+    echo "PREFIX|$line"
+done < sed.log
 
-# while read -r line; do
-#     echo "PREFIX|$line"
-# done < todo1.txt
-
-echo "-----sed-----"
-sed -i 's/ccc/ppp/g' sed.log
-echo
+# echo "-----sed-----"
+# sed -i 's/ccc/ppp/g' sed.log
+# echo
 
 echo "-----awk-----"
 ps aux | grep python | awk 'BEGIN {print "USER PID"} {print $1, $2, $3}'
 
+echo "-----grep-----"
+type=INFO
+cat sed.log | grep "$type" | echo "$type: $(wc -l)" # 2
+
+echo "-----touch-----"
+# touch text1.txt text2.txt
+
+echo "-----for & files 1-----"
+for file in *.txt; do
+    echo "$file"
+done
+
+echo "-----for & files 2-----"
+# for file in *.txt; do
+#     mv "$file" "$(whoami)_$file"
+# done
+
+echo "-----for & files 3-----"
+# for file in *.txt; do
+#     mv "$file" "${file%.txt}.log"
+# done
+
+# # Remove any extension
+# ${file%.*}
+# # Replace extension
+# ${file%.*}.new
+# # Remove multiple dots (e.g., file.tar.gz)
+# ${file%%.tar.gz}.log
+
+echo "-----find-----"
+find ./ -type f -name "*e*" 
+
+echo "-----here doc-----"
+bash << EOF
+    func1(){
+        echo "⚠️  here doc"      
+    }
+    func1
+EOF
+
+echo "-----func with \$-----"
+func1(){
+        echo "\$1: $1"
+        echo "\$2: $2"
+        echo "num of param $#"
+        echo "params: $@"     
+        echo "params: $*"     
+}
+func1 123123 "Cat"
+
+echo "-----here doc-----"
+cut -c1-80 ps_log.logs | head -n 5
+
+echo "-----ls----------"
+ls -lS | grep '^-'
+
+echo "-----if -f ------"
+if [[ -f "sed1.log" ]];then
+    echo "File exists ✅ "
+else
+    echo "File does not exists ❌"
+fi
+
+echo "-----ssh ------"
+
+echo "-----sshpass ------"
+
+echo "-----ssh keygen ------"
+
+echo "-----curl ------"
+
+echo "-----wget ------"
+# wget -O custom_name.txt http://localhost:8121/m_file
+
+# 2>
+# uname -a
+# cmp, diff
+# uptime -p
+# id -un
+# env | grep 'HOME\|PATH'
+# history
+# ip route
+# ⚠️ tcpdump
+# ⚠️ Wireshark
+# apt list --installed 
+
+# != → string not equal
+# -ne → numeric not equal (integers)
+# && : AND
+# || : OR
+# ! : NOT
+# -eq equal
+# -ne not equal
+# -gt greater than
+# -ge greater or equal
+# -lt less than
+# -le less or equal
